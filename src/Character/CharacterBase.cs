0e8195cc8a9b81da0748560cc9aca2aa92649c7c
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-11 15:40:03 -0300

    Create CharacterBase.cs

---
+++ src/Character/CharacterBase.cs
@@ -0,0 +1,11 @@
+namespace ProjectAzura.src.Character
+{
+    /// <summary>
+    /// Base class for characters with dialogue options.
+    /// </summary>
+    // Due to characters not being units, we need to create them here, separate from the RPGSystem.
+    // Wait, should this be an interface? idk.
+    public class CharacterBase
+    {
+    }
+}
