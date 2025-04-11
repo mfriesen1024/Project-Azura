60f4cd4f7ac9c3dfcd67e8a0a68b482285720bde
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-11 15:45:10 -0300

    Update CharacterBase.cs

--- src/Character/CharacterBase.cs
+++ src/Character/CharacterBase.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ProjectAzura.src.Character
 {
     /// <summary>
@@ -5,7 +7,13 @@ namespace ProjectAzura.src.Character
     /// </summary>
     // Due to characters not being units, we need to create them here, separate from the RPGSystem.
     // Wait, should this be an interface? idk.
-    public class CharacterBase
+    public abstract class CharacterBase
     {
+        protected string[][] dialogue;
+
+        public virtual void PlayDialogue(int index)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
