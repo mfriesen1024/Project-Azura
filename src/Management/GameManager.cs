ed2306c4588d85a5dd5ef10a993dcd2b715fc09c
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-13 14:10:30 -0300

    Create GameManager.cs

---
+++ src/Management/GameManager.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace ProjectAzura.src.Management
+{
+    /// <summary>
+    /// Responsible for a lot of core functions including the UI.
+    /// </summary>
+    partial class GameManager : Node
+    {
+    }
+}
