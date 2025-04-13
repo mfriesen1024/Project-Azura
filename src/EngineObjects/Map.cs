53be11a5f574b031b5481e9b2c011e545f1c2693
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-13 17:13:59 -0300

    Update Map.cs

--- src/EngineObjects/Map.cs
+++ src/EngineObjects/Map.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Godot;
+using RPGSystem.Encounter;
 
 namespace ProjectAzura.src.EngineObjects
 {
-    class Map
+    partial class Map : TileMapLayer
     {
+        // The upper and lower bounds
+        [Export(PropertyHint.None, "The -x -y corner of the map.")] Vector2I lbound;
+        [Export(PropertyHint.None, "The -x -y corner of the map.")] Vector2I ubound;
+        [Export(PropertyHint.ResourceType, "HazardTable")] HazardTable hazardTable;
+        public Area InternalMapData { get; protected set; }
+
+
+        public override void _Ready()
+        {
+            base._Ready();
+        }
     }
 }
