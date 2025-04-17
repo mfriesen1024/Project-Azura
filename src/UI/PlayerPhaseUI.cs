80c9136783403cefd0caa690fb419fbe7a05da7d
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-17 11:07:17 -0300

    Add movement stuff.

--- src/UI/PlayerPhaseUI.cs
+++ src/UI/PlayerPhaseUI.cs
@@ -7,12 +7,27 @@ namespace ProjectAzura.src.UI
     {
         internal Ship FocusedShip { get; set; }
         [Export] Camera2D camera;
+        [Export] Sprite2D cursorMovableElement;
+        private const string up="up";
+        private const string down="down";
+        private const string left="left";
+        private const string right="right";
 
         public override void _EnterTree()
         {
             base._EnterTree();
 
             camera.Position = FocusedShip.Sprite.Position;
+        }
+
+        public override void _UnhandledKeyInput(InputEvent @event)
+        {
+            base._UnhandledKeyInput(@event);
+
+            if (Input.IsActionJustPressed(up)) { camera.Position += new Vector2(0, -32); }
+            if (Input.IsActionJustPressed(down)) { camera.Position += new Vector2(0, 32); }
+            if (Input.IsActionJustPressed(left)) { camera.Position += new Vector2(-32, 0); }
+            if (Input.IsActionJustPressed(right)) { camera.Position += new Vector2(32, 0); }
         }
     }
 }
