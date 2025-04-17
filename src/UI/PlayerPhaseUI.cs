d08469d979a5145aea08cd76afbbbfb681ceaee3
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-17 17:05:25 -0300

    Add action availability logic.

--- src/UI/PlayerPhaseUI.cs
+++ src/UI/PlayerPhaseUI.cs
@@ -1,5 +1,7 @@
 using Godot;
+using ProjectAzura.src.Character;
 using ProjectAzura.src.Entity;
+using System;
 
 namespace ProjectAzura.src.UI
 {
@@ -8,7 +10,8 @@ namespace ProjectAzura.src.UI
         internal Ship FocusedShip { get; set; }
         [Export] Camera2D camera;
         [Export] Sprite2D cursorMovableElement;
-        [Export] ShipStatsDisplay currentShip, opposingShip;
+        [Export] ShipStatsDisplay currentShip, targetShip;
+        [Export] Button move, attack, repair, brace, pass;
 
         private const string up = "ui_up";
         private const string down = "ui_down";
@@ -20,6 +23,7 @@ namespace ProjectAzura.src.UI
             base._EnterTree();
 
             camera.Position = FocusedShip.Sprite.Position;
+            UpdateAvailableActions();
         }
 
         public override void _UnhandledKeyInput(InputEvent @event)
@@ -30,6 +34,33 @@ namespace ProjectAzura.src.UI
             if (Input.IsActionJustPressed(down)) { camera.Position += new Vector2(0, 32); }
             if (Input.IsActionJustPressed(left)) { camera.Position += new Vector2(-32, 0); }
             if (Input.IsActionJustPressed(right)) { camera.Position += new Vector2(32, 0); }
+        }
+
+        void UpdateAvailableActions()
+        {
+            // First disable everything.
+            move.Disabled = true;
+            attack.Disabled = true;
+            brace.Disabled = true;
+            repair.Disabled = true;
+
+            foreach (CrewMember cm in FocusedShip.Crew)
+            {
+                if (!cm.HasActed)
+                {
+                    foreach (ActionType at in cm.AvailableActions)
+                    {
+                        switch (at)
+                        {
+                            case ActionType.Move: move.Disabled = FocusedShip.hasMoved; break;
+                            case ActionType.Attack: attack.Disabled = false; break;
+                            case ActionType.Brace: brace.Disabled = false; break;
+                            case ActionType.Repair: repair.Disabled = false; break;
+                            default: GD.PrintErr(new NotImplementedException($"{at} is not implemented in playerphase ui")); break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
