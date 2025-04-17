5b7f8889b05edf97c6de238520c3497f6a65a89f
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-17 10:52:37 -0300

    Add initialization logic to PlayerPhaseUI

--- src/UI/PlayerPhaseUI.cs
+++ src/UI/PlayerPhaseUI.cs
@@ -1,8 +1,18 @@
 using Godot;
+using ProjectAzura.src.Entity;
 
 namespace ProjectAzura.src.UI
 {
     public partial class PlayerPhaseUI:Control
     {
+        internal Ship FocusedShip { get; set; }
+        [Export] Camera2D camera;
+
+        public override void _EnterTree()
+        {
+            base._EnterTree();
+
+            camera.Position = FocusedShip.Sprite.Position;
+        }
     }
 }
