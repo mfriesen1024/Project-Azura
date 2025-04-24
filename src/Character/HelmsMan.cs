09e899a476980633b5abbcbe5341b14d4262d023
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2025-04-23 23:03:52 -0300

    Update crew constructors.

--- src/Character/HelmsMan.cs
+++ src/Character/HelmsMan.cs
@@ -9,11 +9,21 @@ namespace ProjectAzura.src.Character
     {
         public HelmsMan()
         {
+            Init();
+        }
+
+        public HelmsMan(ActionType[] availableActions, float powerModifier) : base(availableActions, powerModifier)
+        {
+            Init();
+        }
+
+        private void Init()
+        {
             var actions = AvailableActions.ToList();
-            actions.Add(ActionType.Attack);
+            actions.Add(ActionType.Move);
             AvailableActions = actions.ToArray();
 
-            PowerModifier = 1.2f;
+            PowerModifier *= 1.2f;
         }
     }
 }
